5340e28f38847feeb9054b8ce2767ca2afed3591
Aditya Khullar <aditya.khullar@nagarro.com>
2019-04-30 11:52:58 +0530

    Removed unused imports

--- app/src/androidTest/java/com/test/ActivityTest.java
+++ app/src/androidTest/java/com/test/ActivityTest.java
@@ -2,7 +2,6 @@ package com.test;
 
 
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,10 +11,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
